d7621f6f539d7f174198cf9b67d57d28857498c3
coderxuz <coderxuz>
2024-07-28 19:36:25 +0500

    chatted

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,57 @@
-const name = document.querySelector('.name')
-const content = document.querySelector('.content')
-const form= document.querySelector('form')
+const nameDiv = document.querySelector(".name");
+const content = document.querySelector(".content");
+const form = document.querySelector("form");
 
+const get = async (resurs) => {
+  const request = await fetch(resurs);
+  const data = await request.json();
+  return data;
+};
+nameDiv.innerHTML = `<h1>${localStorage.getItem("user")}</h1>`;
+const post = async (url, data = {}) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+  if (!form.text.value) {
+    e.preventDefault();
+  } else {
+    const text = {
+      user: localStorage.getItem("user"),
+      text: form.text.value,
+    };
+    post("http://localhost:3000/chat", text);
+    textshow();
+  }
+});
+window.addEventListener("DOMContentLoaded", textshow());
+function textshow() {
+  get("http://localhost:3000/chat").then((data) => {
+    const dataArr = Array.from(data);
+    dataArr.map((item) => {
+      const box = document.createElement("div");
+      const boxInner = document.createElement("div");
+      content.appendChild(box);
+      box.appendChild(boxInner);
+      box.classList = "box";
+      boxInner.classList = "box-inner";
+      boxInner.id = item.user;
+      boxInner.innerHTML = `<p>${item.text}</p>`;
+      if (item.user === localStorage.getItem("user")) {
+        boxInner.style.right = "0";
+      } else {
+        boxInner.style.left = "0";
+      }
+    });
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,29 +1,35 @@
-const form = document.querySelector('form')
-const get = async(resurs)=>{
-    const request = await fetch(resurs)
-    const data = await request.json()
-    return data
-}
-const userValue = localStorage.getItem('user')
-const check = (value)=>{
-        get('http://localhost:3000/users').then((data)=>{
-            const dataArr = Array.from(data)
-            if(value){
-                window.open('./chat.html', '_self')
-            }
-            else{
+const form = document.querySelector("form");
+const get = async (resurs) => {
+  const request = await fetch(resurs);
+  const data = await request.json();
+  return data;
+};
+const userValue = localStorage.getItem("user");
+const check = (value) => {
+  get("http://localhost:3000/users")
+    .then((data) => {
+      const dataArr = Array.from(data);
+      console.log(data);
+      dataArr.map((user) => {
+        if (user.user === value) {
+          window.open("./chat.html", "_self");
+        } else {
+          form.addEventListener("submit", (e) => {
+            e.preventDefault();
 
-                form.addEventListener('submit', (e)=>{
-                    e.preventDefault()
-                    dataArr.map((user)=>{
-                        
-                        if(form.user.value === user.user && form.password.value === user.password){
-                           window.open('./chat.html', '_self')
-                           localStorage.setItem('user', user.user)
-                        }
-                    })
-                })
+            if (
+              form.user.value === user.user &&
+              form.password.value === user.password
+            ) {
+              window.open("./chat.html", "_self");
+              localStorage.setItem("user", user.user);
             }
-        })
-}
-window.addEventListener('DOMContentLoaded', check(userValue))
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+window.addEventListener("DOMContentLoaded", check(userValue));
